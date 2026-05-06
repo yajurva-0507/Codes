6c63cc6bbe7fe44028173a6891d3475395cd6410
yajurva-0507 <yajurvsoni@gmail.com>
2026-05-06 21:03:08 +0530

    Add files via upload

---
+++ avg.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+c=int(input("Enter the third number: "))    
+d=int(input("Enter the fourth number:"))
+e=int(input("Enter the fifth number:"))
+avg=(a+b+c+d+e)/5
+print("The average of the five numbers is: ", avg)
