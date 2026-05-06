7024fdb9a561d3b9e239e42a1b8fbe83934aac3b
yajurva-0507 <yajurvsoni@gmail.com>
2026-05-06 21:02:58 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,9 @@
+#area of circle
+x=float(input("Enter the radius:"))
+a=3.14*x**2
+print("Area of given circle is: ",a)
+#area of rectangle
+p=float(input("Enter length: "))
+q=float(input("Enter breadth: "))
+b=p*q
+print("Area of given rectangle is: ",b)
