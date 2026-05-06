83239f62eb6cd51edc4e773b7f4407f39efa9b60
yajurva-0507 <yajurvsoni@gmail.com>
2026-05-06 21:04:11 +0530

    Add files via upload

---
+++ DictOperations.py
@@ -0,0 +1,10 @@
+dict={"name":"Sharayu","age":12,"city":"Pune","state":"Maharashtra"}
+print(dict)
+print(dict["city"])
+print(len(dict))
+print(type(dict))
+print(dict.keys())
+print(dict.values())
+print(dict.items())
+for i in dict:
+    print(i)
